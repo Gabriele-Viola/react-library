4b2864e0766899d107d762b0263e4d731d5ef15b
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-11-25 10:24:14 +0100

    add routers

---
+++ src/pages/Books.jsx
@@ -0,0 +1,24 @@
+export default function Books() {
+    return (
+        <>
+            <header>
+                <div className="logo">
+                    logo
+                </div>
+                <nav>
+                    <a href="/">Home</a>
+                    <a href="/books">Books</a>
+                    <a href="/About">About</a>
+                    <a href="/Contacts">Contacts</a>
+                </nav>
+            </header>
+            <main>
+                <h1>Books</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae qui nesciunt laboriosam possimus, eum dicta.</p>
+            </main>
+            <footer>
+                copyright 2024
+            </footer>
+        </>
+    )
+}
---
+++ src/pages/home.jsx
@@ -0,0 +1,24 @@
+export default function Home() {
+    return (
+        <>
+            <header>
+                <div className="logo">
+                    logo
+                </div>
+                <nav>
+                    <a href="/">Home</a>
+                    <a href="/">Books</a>
+                    <a href="/About">About</a>
+                    <a href="/Contacts">Contacts</a>
+                </nav>
+            </header>
+            <main>
+                <h1>HomePage</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae qui nesciunt laboriosam possimus, eum dicta.</p>
+            </main>
+            <footer>
+                copyright 2024
+            </footer>
+        </>
+    )
+}
